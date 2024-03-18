af201f3a65101ff40c25cc41cccb28d43d5e37d2
Tobias Steinbach <tobias.steinbach@stud.tu-darmstadt.de>
2024-03-18 15:54:08 +0100

    Adjusted timer in Arduino project so they produce correct periods. Code reworks for better readability.

--- signal_gen_arduino/sine_gen/sine_gen.ino
+++ signal_gen_arduino/sine_gen/sine_gen.ino
@@ -1,159 +1,119 @@
-const int fixedFrequencyLow = 5; // Constant frequency for pin 3 in Hz
-const int fixedFrequencyHigh = 15; // Constant frequency for pin 5 in Hz
+/***
+@author Tobias Steinbach
+Part of the project seminar "Contactless Blood Oxygen Detection"
 
-const int pwmPinLow = 5; // low PWM output pin
-const int pwmPinHigh = 6; // high PWM output pin
-const float sineSteps =  100.0;
-const float maxSineValue = 2.0*PI; //360.0; //2pi
-const int waitPeriod = 500; // us
+  This code can be used to create 2 independent PWM outputs on pins 5 and 6 of an Arduino Uno.
+  A sine wave is generated with a period length based on the fixed_frequency_XX variables.
+  Since this is inteded to be used with orange and infrared LED arrays, the naming is _OR and _IR respectively.
+  The frequencies can be whole numbers from 1 to 20.
+  To create the signal the Timers 1 and 2 of the Arduino are used.
+  Information about the state of the program can be printed out with the serial interface.
+***/
 
-const float holdLimitLow = 1. / (fixedFrequencyLow * waitPeriod * 0.000001 * sineSteps);
-const float holdLimitHigh = 1. / (fixedFrequencyHigh * waitPeriod * 0.000001 * sineSteps);
+const int fixed_frequency_IR = 5; // Constant frequency for pin 3 in Hz
+const int fixed_frequency_OR = 10; // Constant frequency for pin 5 in Hz
 
+const int pwm_pin_IR = 5; // low PWM output pin
+const int pwm_pin_OR = 6; // high PWM output pin
+const float sine_steps =  100.0; // define resolution of sine wave
 
-int sineCounterLow = 0;
-int sineCounterHigh = 0;
-int holdCounterLow = 0;
-int holdCounterHigh = 0;
+int sine_counter_IR = 0; // Used to compute sine values from steps 1 to 100
+int sine_counter_OR = 0;
 
-float sinValueLow = 0;
-float sinValueHigh = 0;
-long dutyCycleLow = 0;
-long dutyCycleHigh = 0;
+int limit_timer_1 = 0; // Limits for the Timer used for counting
+int limit_timer_2 = 0;
 
-long curr_time = 0.;
-long last_period_time = 0.;
+unsigned long last_period_time_IR = 0;
+unsigned long last_period_time_OR =0;
 
-/*void setup() {
-  pinMode(pwmPinLow, OUTPUT);
-  digitalWrite(pwmPinLow, LOW);
-  pinMode(pwmPinHigh, OUTPUT);
-  digitalWrite(pwmPinHigh, LOW);
-  Serial.begin(115200); 
-  Serial.print("Start PWM with low frequency ");
-  Serial.print(fixedFrequencyLow);
-  Serial.print(" Hz and high frequency ");
-  Serial.print(fixedFrequencyHigh);  
-  Serial.print(" Hz\n\r");
-  last_period_time = micros();
-}
-
-void loop() {
-  curr_time = micros() - last_period_time;
-  if (curr_time > dutyCycleLow)        // we are in the last 236ms of the wave, vh should be low
-    digitalWrite(pwmPinLow, LOW);
-  else                         // we are in the first 14ms of the wave, vh should be high
-    digitalWrite(pwmPinLow, HIGH);
-
-  if (curr_time > dutyCycleHigh)        // we are in the last 236ms of the wave, vh should be low
-    digitalWrite(pwmPinHigh, LOW);
-  else                         // we are in the first 14ms of the wave, vh should be high
-    digitalWrite(pwmPinHigh, HIGH);
-
-  if (curr_time > waitPeriod) {
-    // we should start a new period
-
-    // Generate low sine wave
-    if(holdCounterLow < holdLimitLow) {
-      holdCounterLow++;   
-    } else {
-      holdCounterLow = 0;
-      sineCounterLow = (sineCounterLow < sineSteps) ? (sineCounterLow + 1) : 0; 
-      //sinValueLow = sin(radians((float)sineCounterLow / sineSteps * maxSineValue));
-      sinValueLow = sin(sineCounterLow / sineSteps * maxSineValue);
-      // scale the sine value so it fits the range
-      dutyCycleLow = (sinValueLow + 1) / 2 * waitPeriod;
-    }
-
-    // Generate high sine wave
-    if(holdCounterHigh < holdLimitHigh) {
-      holdCounterHigh++;   
-    } else {
-      holdCounterHigh = 0;
-      sineCounterHigh = (sineCounterHigh < sineSteps) ? (sineCounterHigh + 1) : 0; 
-      sinValueHigh = sin(radians((float)sineCounterHigh / sineSteps * maxSineValue));
-      // scale the sine value so it fits the range
-      dutyCycleHigh = (sinValueHigh + 1) / 2 * waitPeriod;
-    }
-
-    last_period_time = micros();    
-  } 
-}*/
-/*
-const int pwmPin = 3; // PWM output pin
-const int fixedFrequency = 1; // Constant frequency in Hz
-const int waitDelay = 1./ fixedFrequency * 1000000 / 360; // delay between pwm update in us
-
-void setup() {
-  pinMode(pwmPin, OUTPUT);
-  Serial.begin(115200); 
-  Serial.print("Start PWM with frequency ");
-  Serial.print(fixedFrequency);
-  Serial.print(" Hz\n\r");
-}
-
-void loop() {
-  // Generate a sine wave
-  for (int i = 0; i < 360; i++) {
-    float sinValue = sin(radians(i)); // Calculate sine value
-    int dutyCycle = (sinValue + 1) / 2 * 255; // Map sine value to PWM duty cycle
-    analogWrite(pwmPin, dutyCycle); // Update PWM output
-    delayMicroseconds(waitDelay); // Adjust delay for desired frequency
-  }
-}
-*/
-int limit1 = 0;
-int limit2 = 0;
-
+// ISR for handling timer events that update the IR PWM
 ISR(TIMER1_COMPA_vect)
 {
-  int dutyCycle = (sin(radians(sineCounterLow*3.6)) + 1) / 2 * 255; // Map sine value to PWM duty cycle
-  analogWrite(pwmPinLow, dutyCycle);
-  if(sineCounterLow >= sineSteps) {
-    sineCounterLow = 0;
+  // Calculate new duty cycle based on counter
+  int dutyCycle = (sin(radians(sine_counter_IR*3.6)) + 1) / 2 * 255; // Map sine value to PWM duty cycle
+  analogWrite(pwm_pin_IR, dutyCycle);
+  if(sine_counter_IR >= (sine_steps-1)) {
+    // Use this to debug period time
+    /*Serial.print(micros() - last_period_time_IR);  
+    Serial.print(" µs in low frequency\n\r");
+    last_period_time_IR = micros();*/
+    sine_counter_IR = 0;   
   } else {
-    sineCounterLow++;
+    sine_counter_IR++;
   }
-  OCR1A += limit1;
+  OCR1A += limit_timer_1;  
 }
 
+// ISR for handling timer events that update OR PWM
 ISR(TIMER2_COMPA_vect)
 {
-  int dutyCycle = (sin(radians(sineCounterHigh*3.6)) + 1) / 2 * 255; // Map sine value to PWM duty cycle
-  analogWrite(pwmPinHigh, dutyCycle);
-  if(sineCounterHigh >= sineSteps) {
-    sineCounterHigh = 0;
+  // Calculate new duty cycle based on counter
+  int dutyCycle = (sin(radians(sine_counter_OR*3.6)) + 1) / 2 * 255; // Map sine value to PWM duty cycle
+  analogWrite(pwm_pin_OR, dutyCycle);
+  if(sine_counter_OR >= (sine_steps-1)) {
+    // Use this to debug period time
+    /*Serial.print(micros() - last_period_time_OR);  
+    Serial.print(" µs in high frequency\n\r");
+    last_period_time_OR = micros();*/
+    sine_counter_OR = 0;    
   } else {
-    sineCounterHigh++;
+    sine_counter_OR++;
   }
-  OCR2A += limit2;
+  OCR2A += limit_timer_2;
 }
 
+// Initialize program
 void setup() {
-  pinMode(pwmPinLow, OUTPUT);
-  pinMode(pwmPinHigh, OUTPUT);
 
-  // Configure Timer1 for PWM generation on pwmPinLow
+  pinMode(pwm_pin_IR, OUTPUT);
+  pinMode(pwm_pin_OR, OUTPUT);
+  // Print out general information via UART
+  Serial.begin(115200); 
+  Serial.print("Start PWM with low frequency ");
+  Serial.print(fixed_frequency_IR);
+  Serial.print(" Hz and high frequency ");
+  Serial.print(fixed_frequency_OR);  
+  Serial.print(" Hz\n\r");
+
+  // Configure Timer1 for PWM generation on pwm_pin_IR
   TCCR1A = 0;           // Init Timer1A
   TCCR1B = 0;           // Init Timer1B
-  TCCR1B |= B00000010;  // Prescaler = 1
-  int prescaler1 = 8;
-  int fixedPeriodLow = 1 / fixedFrequencyLow;
-  limit1 = 16000000 / (prescaler1 * fixedFrequencyLow * sineSteps) - 1;
-  OCR1A = limit1;  // Timer Compare1A Register
+  // Adjust prescaler based on desired frequency
+  int prescaler_timer_1 = 0;
+  if (fixed_frequency_IR < 15) {
+    prescaler_timer_1 = 8;
+    TCCR1B |= B00000010;
+    
+  } else {
+    prescaler_timer_1 = 1;
+    TCCR1B |= B00000001;
+  }
+  limit_timer_1 = 16000000 / (prescaler_timer_1 * fixed_frequency_IR * sine_steps) - 1;
+  OCR1A = limit_timer_1;  // Timer Compare1A Register
   TIMSK1 |= B00000010;  // Enable Timer COMPA Interrupt
   
-  // Configure Timer2 for PWM generation on pwmPinHigh
-  TCCR2A = 0;           // Init Timer1A
-  TCCR2B = 0;           // Init Timer1B
-  TCCR2B |= B00000111;  // Prescaler = 1
-  int prescaler2 = 1024;
-  limit2 = 16000000 / (prescaler2 * fixedFrequencyHigh * sineSteps) - 1;
-  OCR2A = limit2;  // Timer Compare1A Register
+  // Configure Timer2 for PWM generation on pwm_pin_OR
+  TCCR2A = 0; // Init Timer2A
+  TCCR2B = 0;
+  // Adjust Prescaler based on desired frequency
+  int prescaler_timer_2 = 0;  // Init Timer2B
+  if (fixed_frequency_OR < 3) {
+    TCCR2B |= B00000111;
+    prescaler_timer_2 = 1024;
+  }
+  else if (fixed_frequency_OR < 6) {
+    TCCR2B |= B00000110;
+    prescaler_timer_2 = 256;
+  }
+  else {
+    TCCR2B |= B00000101;
+    prescaler_timer_2 = 128;
+  }
+  limit_timer_2 = 16000000 / (prescaler_timer_2 * fixed_frequency_OR * sine_steps) - 1;
+  OCR2A = limit_timer_2;  // Timer Compare1A Register
   TIMSK2 |= B00000010;  // Enable Timer COMPA Interrupt
-
 }
 
 void loop() {
-  // Your main code here
+  // Empty for now
 }
